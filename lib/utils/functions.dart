c6315b552e43b1ecf903a5ca21dc5e91c9346655
Rodger <rkumwanje@gmail.com>
2021-09-21 18:00:41 +0200

    50 code values done

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,1 +1,105 @@
-import 'package:flutter/material.dart';
+String visibility(double value) {
+  if (value < 100) {
+    return '00';
+  } else if (value >= 100 && value < 200) {
+    return '01';
+  } else if (value >= 100 && value < 300) {
+    return '02';
+  } else if (value >= 300 && value < 400) {
+    return '03';
+  } else if (value >= 400 && value < 500) {
+    return '04';
+  } else if (value >= 500 && value < 600) {
+    return '05';
+  } else if (value >= 600 && value < 700) {
+    return '06';
+  } else if (value >= 700 && value < 800) {
+    return '07';
+  } else if (value >= 800 && value < 900) {
+    return '08';
+  } else if (value >= 900 && value < 1000) {
+    return '09';
+  } else if (value >= 1000 && value < 1100) {
+    return '10';
+  } else if (value >= 1100 && value < 1200) {
+    return '11';
+  } else if (value >= 1200 && value < 1300) {
+    return '12';
+  } else if (value >= 1300 && value < 1400) {
+    return '13';
+  } else if (value >= 1400 && value < 1500) {
+    return '14';
+  } else if (value >= 1500 && value < 1600) {
+    return '15';
+  } else if (value >= 1600 && value < 1700) {
+    return '16';
+  } else if (value >= 1700 && value < 1800) {
+    return '17';
+  } else if (value >= 1800 && value < 1900) {
+    return '18';
+  } else if (value >= 1900 && value < 2000) {
+    return '19';
+  } else if (value >= 2000 && value < 2100) {
+    return '20';
+  } else if (value >= 2100 && value < 2200) {
+    return '21';
+  } else if (value >= 2200 && value < 2300) {
+    return '22';
+  } else if (value >= 2300 && value < 2400) {
+    return '23';
+  } else if (value >= 2400 && value < 2500) {
+    return '24';
+  } else if (value >= 2500 && value < 2600) {
+    return '25';
+  } else if (value >= 2600 && value < 2700) {
+    return '26';
+  } else if (value >= 2700 && value < 2800) {
+    return '27';
+  } else if (value >= 2800 && value < 2900) {
+    return '28';
+  } else if (value >= 2900 && value < 3000) {
+    return '29';
+  } else if (value >= 3000 && value < 3100) {
+    return '30';
+  } else if (value >= 3100 && value < 3200) {
+    return '31';
+  } else if (value >= 3200 && value < 3300) {
+    return '32';
+  } else if (value >= 3300 && value < 3400) {
+    return '33';
+  } else if (value >= 3400 && value < 3500) {
+    return '34';
+  } else if (value >= 3500 && value < 3600) {
+    return '35';
+  } else if (value >= 3600 && value < 3700) {
+    return '36';
+  } else if (value >= 3700 && value < 3800) {
+    return '37';
+  } else if (value >= 3800 && value < 3900) {
+    return '38';
+  } else if (value >= 3900 && value < 4000) {
+    return '39';
+  } else if (value >= 4000 && value < 4100) {
+    return '40';
+  } else if (value >= 4100 && value < 4200) {
+    return '41';
+  } else if (value >= 4200 && value < 4300) {
+    return '42';
+  } else if (value >= 4300 && value < 4400) {
+    return '43';
+  } else if (value >= 4400 && value < 4500) {
+    return '44';
+  } else if (value >= 4500 && value < 4600) {
+    return '45';
+  } else if (value >= 4600 && value < 4700) {
+    return '46';
+  } else if (value >= 4700 && value < 4800) {
+    return '47';
+  } else if (value >= 4800 && value < 4900) {
+    return '48';
+  } else if (value >= 4900 && value < 5000) {
+    return '49';
+  } else if (value == 5000) {
+    return '50';
+  }
+}
